1aeee4deb9d3896f1d4dd8185c02de3f0d331849
Vince Carey <stvjc@channing.harvard.edu>
2013-09-17 00:31:40 -0400

    more material in Annotation

---
+++ utils/laconv.R
@@ -0,0 +1,29 @@
+laconv = function(x) {
+ x = gsub("\\\\section\\{(.*)\\}", "<h2>\\1</h2>", x)
+ x = gsub("\\\\texttt\\{(.*)\\}", "<code>\\1</code>", x)
+ x = gsub("\\\\textit\\{(.*)\\}", "<emph>\\1</emph>", x)
+ x = gsub("\\\\textbf\\{(.*)\\}", "<emph>\\1</emph>", x)
+ x = gsub("\\\\verb+(.*)+\\}", "<code>\\1</code>", x)
+ x = gsub("\\\\emph\\{(.*)\\}", "<emph>\\1</emph>", x)
+ x = gsub("\\\\subsection\\{(.*)\\}", "<h3>\\1</h3>", x)
+ x = gsub("\\\\subsection.\\{(.*)\\}", "<h3>\\1</h3>", x)
+ x = gsub("\\\\subsubsection\\{(.*)\\}", "<h4>\\1</h4>", x)
+ x = gsub("\\\\subsubsection.\\{(.*)\\}", "<h4>\\1</h4>", x)
+ x = gsub("\\\\begin\\{itemize\\}", "<ul>", x)
+ x = gsub("\\\\begin\\{verbatim\\}", "<code>", x)
+ x = gsub("\\\\begin\\{center\\}", " ", x)
+ x = gsub("\\\\end\\{center\\}", " ", x)
+ x = gsub("\\\\setkey.*", " ", x)
+ x = gsub("\\\\end\\{verbatim\\}", "</code>", x)
+ x = gsub("\\\\end\\{itemize\\}", "</ul>", x)
+ x = gsub("\\\\item ", "<li>", x)
+ x = gsub("<<(.*)>>=", "```{r \\1}", x)
+ x = gsub("^@$", "```", x)
+ x
+}
+
+convla = function(fn) {
+  bk = readLines(fn)
+  writeLines(bk, con=paste0(fn, ".bak"))
+  writeLines(laconv(bk), con=paste0(fn, ".Rmd"))
+}
